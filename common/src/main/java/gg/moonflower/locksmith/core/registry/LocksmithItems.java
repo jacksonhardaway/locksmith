709fe2de850a4fcfce6ed919edaffc5fc7faca88
Jackson <jackson.hardaway@gmail.com>
2021-11-24 15:29:25 -0800

    Add blank lock

--- common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithItems.java
+++ common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithItems.java
@@ -12,9 +12,12 @@ import java.util.function.Supplier;
 public class LocksmithItems {
     public static final PollinatedRegistry<Item> ITEMS = PollinatedRegistry.create(Registry.ITEM, Locksmith.MOD_ID);
 
-    public static final Supplier<Item> LOCK = ITEMS.register("lock", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
-    public static final Supplier<Item> KEY = ITEMS.register("key", () -> new KeyItem(new Item.Properties()));
+    public static final Supplier<Item> BLANK_LOCK = ITEMS.register("blank_lock", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final Supplier<Item> BLANK_KEY = ITEMS.register("blank_key", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final Supplier<Item> LOCK = ITEMS.register("lock", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> KEY = ITEMS.register("key", () -> new KeyItem(new Item.Properties()));
+
     public static final Supplier<Item> KEY_RING = ITEMS.register("key_ring", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final Supplier<Item> LOCKPICK = ITEMS.register("lockpick", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 }
