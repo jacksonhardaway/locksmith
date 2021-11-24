7cb51b833f1ebb5f9188b801f3b9862955727b3c
Jackson <jackson.hardaway@gmail.com>
2021-11-24 01:00:41 -0800

    Change locksmithing table properties

--- common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithBlocks.java
+++ common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithBlocks.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
@@ -15,7 +16,7 @@ import java.util.function.Supplier;
 public class LocksmithBlocks {
     public static final PollinatedRegistry<Block> BLOCKS = PollinatedRegistry.create(Registry.BLOCK, Locksmith.MOD_ID);
 
-    public static final Supplier<Block> LOCKSMITHING_TABLE = registerBlock("locksmithing_table", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
+    public static final Supplier<Block> LOCKSMITHING_TABLE = registerBlock("locksmithing_table", () -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(2.5F).sound(SoundType.WOOD)), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
 
     private static Supplier<Block> registerBlock(String id, Supplier<Block> block, Item.Properties properties) {
         Supplier<Block> register = BLOCKS.register(id, block);
