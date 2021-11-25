74e7f419216aa9323c24d9f539688a7489ff0169
Jackson <jackson.hardaway@gmail.com>
2021-11-25 04:43:23 -0800

    Added empty key icon

--- common/src/main/java/gg/moonflower/locksmith/core/Locksmith.java
+++ common/src/main/java/gg/moonflower/locksmith/core/Locksmith.java
@@ -2,16 +2,18 @@ package gg.moonflower.locksmith.core;
 
 import gg.moonflower.locksmith.client.lock.ClientLockManager;
 import gg.moonflower.locksmith.client.screen.LocksmithingTableScreen;
-import gg.moonflower.locksmith.common.item.KeyringItem;
+import gg.moonflower.locksmith.common.menu.LocksmithingTableMenu;
 import gg.moonflower.locksmith.common.network.LocksmithMessages;
 import gg.moonflower.locksmith.common.world.lock.LockManager;
 import gg.moonflower.locksmith.core.registry.LocksmithBlocks;
 import gg.moonflower.locksmith.core.registry.LocksmithItems;
 import gg.moonflower.locksmith.core.registry.LocksmithMenus;
+import gg.moonflower.pollen.api.event.events.registry.RegisterAtlasSpriteEvent;
 import gg.moonflower.pollen.api.platform.Platform;
 import gg.moonflower.pollen.api.registry.ClientRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.inventory.InventoryMenu;
 
 public class Locksmith {
     public static final String MOD_ID = "locksmith";
@@ -24,6 +26,7 @@ public class Locksmith {
 
     public static void onClientInit() {
         ClientRegistries.registerScreenFactory(LocksmithMenus.LOCKSMITHING_TABLE_MENU.get(), LocksmithingTableScreen::new);
+        RegisterAtlasSpriteEvent.event(InventoryMenu.BLOCK_ATLAS).register((atlas, registry) -> registry.accept(LocksmithingTableMenu.EMPTY_SLOT_KEY));
         ClientRegistries.registerItemOverride(LocksmithItems.KEYRING.get(), new ResourceLocation(Locksmith.MOD_ID, "keys"), (stack, level, livingEntity) -> Mth.clamp(4 / 4F, 0, 1));
         ClientLockManager.INSTANCE.init();
     }
