0745db171d7992311362d0bd4ee65f2cdb2d83ec
Jackson <jackson.hardaway@gmail.com>
2021-11-28 16:37:30 -0800

    Use pollen tags

--- common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithTags.java
+++ common/src/main/java/gg/moonflower/locksmith/core/registry/LocksmithTags.java
@@ -1,19 +1,13 @@
 package gg.moonflower.locksmith.core.registry;
 
-import dev.architectury.injectables.annotations.ExpectPlatform;
 import gg.moonflower.locksmith.core.Locksmith;
-import gg.moonflower.pollen.api.platform.Platform;
+import gg.moonflower.pollen.api.registry.TagRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.Tag;
 import net.minecraft.world.level.block.Block;
 
 public class LocksmithTags {
 
-    public static final Tag<Block> LOCKABLE = bindBlock(new ResourceLocation(Locksmith.MOD_ID, "lockable"));
+    public static final Tag<Block> LOCKABLE = TagRegistry.bindBlock(new ResourceLocation(Locksmith.MOD_ID, "lockable"));
 
-    // TODO: Move into pollen
-    @ExpectPlatform
-    public static Tag<Block> bindBlock(ResourceLocation tag) {
-        return Platform.error();
-    }
 }
