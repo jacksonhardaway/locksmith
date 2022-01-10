bd26847bcfb49d888d38552b403bbad2d4997fe8
Ocelot <FinntheRaider@gmail.com>
2022-01-09 21:59:27 -0700

    Removed key ring background

--- common/src/main/java/gg/moonflower/locksmith/client/tooltip/ClientKeyringTooltip.java
+++ common/src/main/java/gg/moonflower/locksmith/client/tooltip/ClientKeyringTooltip.java
@@ -1,68 +1,87 @@
 package gg.moonflower.locksmith.client.tooltip;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
-import gg.moonflower.locksmith.common.item.KeyItem;
-import gg.moonflower.locksmith.common.item.KeyringItem;
 import gg.moonflower.locksmith.common.tooltip.KeyringTooltip;
-import gg.moonflower.locksmith.core.Locksmith;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ClientKeyringTooltip implements ClientTooltipComponent {
 
-    public static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(Locksmith.MOD_ID, "textures/gui/container/keyring_tooltip.png");
+    private static final int PADDING = 2;
 
     private final KeyringTooltip tooltip;
+    private final List<Component> tooltipList;
 
     public ClientKeyringTooltip(KeyringTooltip tooltip) {
         this.tooltip = tooltip;
+        this.tooltipList = new ArrayList<>();
     }
 
     @Override
     public int getHeight() {
-        return 83;
+        List<ItemStack> stacks = this.tooltip.getItems();
+        int y = 0;
+        for (ItemStack stack : stacks) {
+            this.updateTooltip(stack);
+            y += Math.max(this.tooltipList.size() * (PADDING + 9), 18);
+        }
+        return y + Math.max(0, stacks.size() - 1) * PADDING;
     }
 
     @Override
     public int getWidth(Font font) {
-        return 22 + this.tooltip.getItems().stream().mapToInt(stack -> Math.max(font.width(stack.getHoverName()), font.width(KeyItem.isOriginal(stack) ? KeyItem.ORIGINAL : KeyItem.COPY))).max().orElse(0);
+        return 18 + PADDING + this.tooltip.getItems().stream().flatMapToInt(stack -> {
+            this.updateTooltip(stack);
+            return this.tooltipList.stream().mapToInt(font::width);
+        }).max().orElse(0);
     }
 
     @Override
     public void renderText(Font font, int x, int y, Matrix4f matrix4f, MultiBufferSource.BufferSource bufferSource) {
         List<ItemStack> stacks = this.tooltip.getItems();
-        for (int i = 0; i < Math.min(KeyringItem.MAX_KEYS, stacks.size()); i++) {
-            ItemStack stack = stacks.get(i);
-            font.drawInBatch(stack.getHoverName(), x + 22, y + i * 20 + 2, -1, true, matrix4f, bufferSource, false, 0, 15728880);
-            font.drawInBatch(KeyItem.isOriginal(stack) ? KeyItem.ORIGINAL : KeyItem.COPY, x + 22, y + i * 20 + 11, -1, true, matrix4f, bufferSource, false, 0, 15728880);
+        for (ItemStack stack : stacks) {
+            this.updateTooltip(stack);
+
+            for (int i = 0; i < this.tooltipList.size(); i++)
+                font.drawInBatch(this.tooltipList.get(i), x + 18 + PADDING, y + i * (PADDING + 9), -1, true, matrix4f, bufferSource, false, 0, 15728880);
+
+            y += Math.max(this.tooltipList.size() * (PADDING + 9) + PADDING, 18 + PADDING);
         }
     }
 
     @Override
     public void renderImage(Font font, int x, int y, PoseStack poseStack, ItemRenderer itemRenderer, int z) {
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE_LOCATION);
-        GuiComponent.blit(poseStack, x, y, z, 0, 0, 20, 81, 128, 128);
-
         List<ItemStack> stacks = this.tooltip.getItems();
-        for (int i = 0; i < Math.min(KeyringItem.MAX_KEYS, stacks.size()); i++) {
-            ItemStack stack = stacks.get(i);
-            itemRenderer.renderAndDecorateItem(stack, x + 2, y + i * 20 + 2, z);
-            itemRenderer.renderGuiItemDecorations(font, stack, x + 2, y + i * 20 + 2);
+        for (ItemStack stack : stacks) {
+            this.updateTooltip(stack);
 
-            if (i == 0)
-                AbstractContainerScreen.renderSlotHighlight(poseStack, x + 2, y + 2, z);
+            itemRenderer.renderAndDecorateItem(stack, x, y, z);
+            itemRenderer.renderGuiItemDecorations(font, stack, x, y);
+
+            y += Math.max(this.tooltipList.size() * (PADDING + 9) + PADDING, 18 + PADDING);
         }
+    }
+
+    private void updateTooltip(ItemStack stack) {
+        this.tooltipList.clear();
+
+        MutableComponent name = new TextComponent("").append(stack.getHoverName()).withStyle(stack.getRarity().color);
+        if (stack.hasCustomHoverName())
+            name.withStyle(ChatFormatting.ITALIC);
+        this.tooltipList.add(name);
+        stack.getItem().appendHoverText(stack, Minecraft.getInstance().level, this.tooltipList, Minecraft.getInstance().options.advancedItemTooltips ? TooltipFlag.Default.ADVANCED : TooltipFlag.Default.NORMAL);
     }
 }
